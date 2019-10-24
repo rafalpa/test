a5842e5f2278f03100bfca7cf166851d37e6adc3
Rafał Paszta <rpaszta@gmail.com>
2019-10-24 19:28:50 +0200

    changed my solution

--- two_fer.py
+++ two_fer.py
@@ -1,5 +1,6 @@
 def two_fer(name="you"):
-    #return "One for " + name + ", one for me."
-    #return "One for %s, one for me." % name
-    #return "One for {}, one for me.".format(name)
-    return f"One for {name}, one for me."
+    # return "One for {}, one for me.".format(name)
+    if name == "Alice":
+        return f"One for Alice, one for me."
+    else:
+        return f"One for {name}, one for me!!!"
--- two_fer_test.py
+++ two_fer_test.py
@@ -1,8 +1,6 @@
 import unittest
 
 from two_fer import two_fer
-
-
 # Tests adapted from `problem-specifications//canonical-data.json` @ v1.2.0
 
 class TwoFerTest(unittest.TestCase):
